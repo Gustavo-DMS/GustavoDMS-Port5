31394c405fa90578fafbc82d7bc32f5bd865718f
GustavoDMS <pipe.gus2014@gmail.com>
2022-06-09 14:57:40 -0300

    testando UI

--- GUI.py
+++ GUI.py
@@ -1,8 +1,14 @@
+from ast import While
 import sqlite3
 from tkinter import *
+
+from sqlalchemy import false
 # import port_5 
 class Application:
-    def __init__(self, master=None):
+    def __init__(self, master):
+        self.child_window = None
+        self.master = master
+        self.toplevels = 0
         self.containerTitulo = Frame(master)
         self.containerTitulo.pack()
 
@@ -14,7 +20,63 @@ class Application:
         self.titulo.pack()
 
         self.botao1 = Button(self.containerCompliance,text="omegalol")
+        self.botao1['command'] = self.janelaNova
         self.botao1.pack()
+
+        self.botao2 = Button(self.containerCompliance,text="omegalol")
+        self.botao2['command'] = self.janelaNova2
+        self.botao2.pack()
+       
+
+    def janelaNova(self):
+        if not self.child_window:
+            nova = Toplevel(self.master)
+            self.child_window = nova
+            self.containerTitulo2 = Frame(nova)
+            self.containerTitulo2.pack()
+
+            self.containerCompliance2 = Frame(nova)
+            self.containerCompliance2.pack()
+
+            self.titulo2 = Label(self.containerTitulo2,text='janela2')
+            self.titulo2['font'] = ("Consolas", "15", "bold")
+            self.titulo2.pack()
+
+            self.botao1 = Button(self.containerCompliance2,text="omegalol")
+                
+            self.botao1.pack()
+
+            def doSomething():
+                nova.destroy()
+                self.child_window = None
+
+            nova.protocol('WM_DELETE_WINDOW', doSomething)
+
+            
+    def janelaNova2(self):
+        if not self.child_window:
+            nova2 = Toplevel(self.master)
+            self.child_window = nova2
+            self.containerTitulo2 = Frame(nova2)
+            self.containerTitulo2.pack()
+
+            self.containerCompliance2 = Frame(nova2)
+            self.containerCompliance2.pack()
+
+            self.titulo2 = Label(self.containerTitulo2,text='janela2')
+            self.titulo2['font'] = ("Consolas", "15", "bold")
+            self.titulo2.pack()
+
+            self.botao1 = Button(self.containerCompliance2,text="omegalol")
+                
+            self.botao1.pack()
+
+            def doSomething():
+                nova.destroy()
+                self.child_window = None
+
+            nova.protocol('WM_DELETE_WINDOW', doSomething)
+
 
 root = Tk()
 Application(root)
